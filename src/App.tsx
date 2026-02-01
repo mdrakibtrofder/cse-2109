5b98b2c4131a0cc5d54876fda22fc85842b9cefa
Md. Rakib Trofder <mdrakibtrofder@gmail.com>
2026-02-01 08:56:54 +0600

    feature: configuration for deploying at github

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/cse-2109">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    base: "/cse-2109/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
