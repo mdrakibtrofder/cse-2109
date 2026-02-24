352314550d1e88493bf258032554ed60c2201be4
Md. Rakib Trofder <mdrakibtrofder@gmail.com>
2026-02-24 14:30:39 +0600

    update url from course code to course name

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-    base: "/cse-2109/",
+    base: "/computer-fundamentals/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
